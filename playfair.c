50ec5d5042aac639383ad82be8812e774dec963a
shivraj <shivrajnaghera@outlook.com>
2021-08-24 12:09:21 +0530

    Implemented playfair encryption

--- playfair.c
+++ playfair.c
@@ -51,8 +51,6 @@ void prepareMatrix(char *key) {
     }
 }
 
-
-
 void printMatrix() {
     for(int i = 0; i < 5; i++) {
         for(int j = 0; j < 5; j++) {
@@ -61,6 +59,75 @@ void printMatrix() {
         printf("\n");
     }
 }
+
+void encrypt(char *plaintext, char *key) {
+    prepareMatrix(key);
+    // printMatrix();
+
+    for(int i = 0; i < strlen(plaintext); i += 2) {
+        char pair[] = {plaintext[i], plaintext[i + 1]};
+
+        if(pair[1] == '\0')
+            pair[1] = 'X';
+
+        if(pair[0] == pair[1]) {
+            pair[1] = 'X';
+            i--;
+        }
+
+        if(pair[0] == 74)
+            pair[0] = 'I';
+        if(pair[1] == 74)
+            pair[1] = 'I';
+
+
+        // printf("%c%c\n", pair[0], pair[1]);
+
+        int firstCharIndex[2];
+        int secondCharIndex[2];
+
+        for(int i = 0; i <  5; i++) {
+            for(int j = 0; j < 5; j++) {
+                if(matrix[i][j] == pair[0])  {
+                    firstCharIndex[0] = i;
+                    firstCharIndex[1] = j;
+                } else if(matrix[i][j] == pair[1]) {
+                    secondCharIndex[0] = i;
+                    secondCharIndex[1] = j;
+                }
+            }
+        }
+
+        // printf("%d %d\n", firstCharIndex[0], firstCharIndex[1]);
+        // printf("%d %d\n", secondCharIndex[0], secondCharIndex[1]);
+
+        if(firstCharIndex[0] == secondCharIndex[0]) {
+            firstCharIndex[1] = (firstCharIndex[1] + 1) % 5;
+            secondCharIndex[1] = (secondCharIndex[1] + 1) % 5;
+        } else if(firstCharIndex[1] == secondCharIndex[1]) {
+            firstCharIndex[0] = (firstCharIndex[0] + 1) % 5;
+            secondCharIndex[0] = (secondCharIndex[0] + 1) % 5;
+        } else {
+            int temp = firstCharIndex[1];
+            firstCharIndex[1] = secondCharIndex[1];
+            secondCharIndex[1] = temp;
+
+            // printf("first character index: %d %d\n", firstCharIndex[0], firstCharIndex[1]);
+            // printf("Second character index: %d %d\n", secondCharIndex[0], secondCharIndex[0]);
+        }
+
+        pair[0] = matrix[firstCharIndex[0]][firstCharIndex[1]];
+        pair[1] = matrix[secondCharIndex[0]][secondCharIndex[1]];
+        
+        // printf("%c%c\n\n", pair[0], pair[1]);
+
+        plaintext[i] = pair[0];
+        plaintext[i + 1] = pair[1];
+    }
+
+    // printf("%s\n", plaintext);
+}
+
 
 int main() {
     char text[MAX];
@@ -77,8 +144,11 @@ int main() {
     // printf("%s\n", text);
     // printf("%s\n", key);
 
-    prepareMatrix(key);
+    // prepareMatrix(key);
 
-    printMatrix();
+    // printMatrix();
+    encrypt(text, key);
+
+    printf("%s\n", text);
     return 0;
 }
